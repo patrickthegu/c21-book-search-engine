515310bcbeada745a014ed48e47299fa9d4c6d99
Patrick Gu <patrickthegu@gmail.com>
2022-10-19 23:32:16 +1100

    final bugs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://book-admin:p@ssword@cluster0.r0gwuf9.mongodb.net/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/27017/books-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
